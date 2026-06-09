4ab58b908a4059880f26e70f60c3acae3b2b941e
Kerzarin <max2001027@gmail.com>
2026-06-09 16:47:44 +0500

    add appConfig validation schema

---
+++ backend/src/configuration/schemes/appConfig.schema.ts
@@ -0,0 +1,40 @@
+import { Type } from 'class-transformer';
+import {
+  IsEnum,
+  IsFQDN,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export enum AppEnvironment {
+  Development = 'development',
+  Production = 'production',
+  Testing = 'testing',
+}
+
+export class AppConfig {
+  @IsNumber({}, { message: 'The port must be a number' })
+  @Min(1, { message: 'The port cannot be less than 1' })
+  @Max(65535, { message: 'The port cannot be larger than 65535' })
+  @Type(() => Number)
+  port!: number;
+
+  @IsFQDN(
+    { require_tld: false },
+    { message: 'The host must be a valid domain or localhost.' },
+  )
+  host!: string;
+
+  @IsString({ message: 'The environment must be a string' })
+  @IsEnum(AppEnvironment, {
+    message: 'Allowed values for env: development, production, testing',
+  })
+  env!: AppEnvironment;
+
+  @IsString({ message: 'The application name must be a string' })
+  @IsNotEmpty({ message: 'The application name must not be empty' })
+  appName!: string;
+}
