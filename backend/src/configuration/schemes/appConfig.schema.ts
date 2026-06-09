8b914c1ff1edc1c99ae74634377a4be35d9dffd4
Kerzarin <max2001027@gmail.com>
2026-06-09 17:10:40 +0500

    fixed the mapping of the configuration scheme and .env

--- backend/src/configuration/schemes/appConfig.schema.ts
+++ backend/src/configuration/schemes/appConfig.schema.ts
@@ -1,4 +1,4 @@
-import { Type } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 import {
   IsEnum,
   IsFQDN,
@@ -16,24 +16,28 @@ export enum AppEnvironment {
 }
 
 export class AppConfig {
+  @Expose({ name: 'PORT' })
   @IsNumber({}, { message: 'The port must be a number' })
   @Min(1, { message: 'The port cannot be less than 1' })
   @Max(65535, { message: 'The port cannot be larger than 65535' })
   @Type(() => Number)
   port!: number;
 
+  @Expose({ name: 'HOST' })
   @IsFQDN(
     { require_tld: false },
     { message: 'The host must be a valid domain or localhost.' },
   )
   host!: string;
 
+  @Expose({ name: 'NODE_ENV' })
   @IsString({ message: 'The environment must be a string' })
   @IsEnum(AppEnvironment, {
     message: 'Allowed values for env: development, production, testing',
   })
   env!: AppEnvironment;
 
+  @Expose({ name: 'APP_NAME' })
   @IsString({ message: 'The application name must be a string' })
   @IsNotEmpty({ message: 'The application name must not be empty' })
   appName!: string;
